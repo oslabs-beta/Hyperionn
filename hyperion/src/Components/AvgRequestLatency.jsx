40ed3584da6ac5296be27172e899e2266dedece8
Anita Duong <anitaduong98@gmail.com>
2022-08-04 19:36:57 -0400

    fixed avgreqlatency

--- hyperion/src/Components/AvgRequestLatency.jsx
+++ hyperion/src/Components/AvgRequestLatency.jsx
@@ -1,133 +1,4 @@
-// import React, { useState, useEffect } from 'react'
-// import { Box } from '@mui/material';
-// import {
-//   Chart as ChartJS,
-//   CategoryScale,
-//   LinearScale,
-//   PointElement,
-//   LineElement,
-//   Title,
-//   Tooltip,
-//   Legend,
-// } from "chart.js";
-// import { Line } from "react-chartjs-2";
-
-// ChartJS.register(
-//   CategoryScale,
-//   LinearScale,
-//   PointElement,
-//   LineElement,
-//   Title,
-//   Tooltip,
-//   Legend
-// );
-
-// const options = {
-//   responsive: true,
-//   maintainAspectRatio: false,
-//   plugins: {
-//     legend: {
-//       position: "top",
-//     },
-//   },
-// };
-
-// const labels = [
-//   "January",
-//   "February",
-//   "March",
-//   "April",
-//   "May",
-//   "June",
-//   "July",
-//   "August",
-//   "September",
-//   "October",
-//   "November",
-//   "December",
-// ];
-
-// const AvgRequestLatency = () => {
-
-//   const data = {
-//     labels,
-//     datasets: [
-//       {
-//         label: "EXPECTED SAVINGS PER MONTH",
-//         data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-//         borderColor: "rgba(123, 31, 162)",
-//         backgroundColor: "rgba(123, 31, 162, 0.5)",
-//         borderWidth: 1,
-//       },
-//       {
-//         label: "SAVINGS GOAL",
-//         data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-//         borderColor: "rgb(255, 125, 69)",
-//         backgroundColor: "rgb(255, 125, 69, 0.5)",
-//         borderWidth: 1,
-//       },
-//     ],
-//   };
-
-//   return (
-//     <Box>
-//         Average Request Latency
-//     </Box>
-//   )
-// }
-// export default AvgRequestLatency;
-
-
 import React, { useState, useEffect } from 'react'
-
-import { Box, Typography } from '@mui/material';
-import {
-  Chart as ChartJS,
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  Title,
-  Tooltip,
-  Legend,
-} from "chart.js";
-import { Line } from "react-chartjs-2";
-
-ChartJS.register(
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  Title,
-  Tooltip,
-  Legend
-);
-
-const options = {
-  responsive: true,
-  maintainAspectRatio: false,
-  plugins: {
-    legend: {
-      position: "top",
-    },
-  },
-};
-
-const labels = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December",
-];
-
 import { Box } from '@mui/material';
 
 import Chart from 'chart.js/auto';
@@ -167,13 +38,12 @@ import StreamingPlugin from 'chartjs-plugin-streaming';
 //  }
 
 
+
 const AvgRequestLatency = () => {
 
 
   return (
     <Box>
-
-        <Typography className="data-label" sx={{ fontSize: '1.2rem', letterSpacing: '1.5px', textTransform: 'uppercase'}}>Average Request Latency</Typography>
          <Line
         data={{
           datasets: [{
@@ -222,7 +92,6 @@ const AvgRequestLatency = () => {
           }
         }}
       />
-
     </Box>
   )
 }
