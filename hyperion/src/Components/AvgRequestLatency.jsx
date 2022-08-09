e9a635ad658d404b88a4d1d81132aee650265f3b
kngreen <kngreen@umich.edu>
2022-08-08 20:01:11 -0400

    connect button

--- hyperion/src/Components/AvgRequestLatency.jsx
+++ hyperion/src/Components/AvgRequestLatency.jsx
@@ -1,55 +1,39 @@
 import React, { useState, useEffect } from 'react'
 import { Box } from '@mui/material';
-
 import Chart from 'chart.js/auto';
 import { Line} from "react-chartjs-2";
 import 'chartjs-adapter-luxon';
 import StreamingPlugin from 'chartjs-plugin-streaming';
 // import { queryDictionary } from '../Containers/DataContainer.jsx';
-
 //const { avgReqLatencyQuery } = queryDictionary;
 
 
- Chart.register(StreamingPlugin);
+Chart.register(StreamingPlugin);
   
-
+//interval for data fetching
 const pollingInterval = 5000;
 
 const AvgRequestLatency = () => {
   let count = 0; 
   const [zookeepers, setZookeepers] = useState([]);
+  //dataPoints will be the array of objects (that contain new data points) that is returned from the fetch request to the server
   const [dataPoints, setDataPoints] = useState([]);
-  //dataPoints = [ {x: 2, y: 4}, {x: 2, y: 5}]
 
-  //  const [zookeepers, setZookeepers] = useState([{
-  //   label: '', //make more specific by pulling the actual name
-  //   backgroundColor: '',
-  //   borderColor: '',
-  //   fill: false,
-  //   data: [],
-  //  }]);
-
-  //const [label, setLabel] = useState('');
-  //const [xAxis, setXAxis] = useState(Date.now());
-  //const [yAxis, setYAxis] = useState();
-  //const [latencyHistory, setLatencyHistory] = useState({})
   
   useEffect(()=> {
-    // make Initial Fetch on Component Did Mount
+    // make Initial Fetch on Component Did Mount that returns the data that will be passed into makeDataSets function
     initialFetch();
-    // console.log('initial useEffect ');
+    // count is incremented, triggering the next useEffect hook
     count++;
   }, [])
   
-  // const initialFetch = async () => {
-  //   //checks how many zookeepers there are.
-
-  const output = [];
+  // iterate through the returned data in order to determine # of zookeepers. 
   const makeDataSets = zooData => {
+    const output = [];
     for (let i = 0; i < zooData.length; i++){
       let colorVal = Math.floor(Math.random() * 255)
       const obj = {
-        label: zooData[i].instance, //make more specific by pulling the actual name
+        label: zooData[i].instance,
         backgroundColor: `rgba(${colorVal}, ${colorVal}, ${colorVal}, 0.5)`,
         borderColor: `rgb(${colorVal}, ${colorVal}, ${colorVal})`,
         fill: false,
@@ -57,17 +41,18 @@ const AvgRequestLatency = () => {
        }
       output.push(obj);
     }
+    //update zookeepers state with the output array that contains the correct # of objects
     setZookeepers(output);
-    // console.log('output', output);
   }
   
+  //intialFetch is invoked when the page first renders. The returned data from the fetch request is passed into makeDataSets.
   const initialFetch = async () => {
     const response = await fetch('/server/metrics?metric=avgReqLatency');
     const data = await response.json();
-    // console.log('Avg request latency: ', data);
     makeDataSets(data);
   }
 
+  //hook for invoking fetchLatency every set interval. This is initially triggered by count being incremented in the first useEffect hook
   useEffect(() => {
     const interval = setInterval(() => {
       fetchLatency();
@@ -75,27 +60,23 @@ const AvgRequestLatency = () => {
     return () => clearInterval(interval);
   },[count])
   
-  
+  //subsequent fetch requests to the server for fresh avgReqLatency data points
   const fetchLatency = async () =>  {
     const newDataPoints = [];
     const response = await fetch('/server/metrics?metric=avgReqLatency')
     const newData = await response.json();
-    // console.log('new Avg request latency: ', newData);
     for (let i = 0; i < newData.length; i++) {
       newDataPoints.push({x: newData[i].x, y: newData[i].y});
     }
-    // console.log('newDataPoints: ', newDataPoints);
-    // setZookeepers(output);
+    //state is updated with the new data points
     setDataPoints(newDataPoints);
   }
 
 
-  // console.log('zookeeper state', zookeepers);
-  //console.log('output', output);
-
   return (
     <Box>
       <Line
+        //datasets is the state zookeepers array
         data={{
           datasets: zookeepers,
         }}
@@ -112,15 +93,11 @@ const AvgRequestLatency = () => {
             x: {
               type: 'realtime',
               realtime: {
-                // delay: 1000,
                 duration : 200000, //duration = x-axis maximum
                 refresh: 5000,
+                //chart will refresh every 5 seconds, pushing in the new data points from state into the datasets.data property
                 onRefresh: chart => {
                   chart.data.datasets.forEach((zooKeeperInstance, index, array) => {
-                    // console.log('zookeeper instance', zooKeeperInstance.data.dataSets);
-                    //console.log('dataPoints[index].x', dataPoints[index].x);
-                    //console.log('dataPoints[index].y', dataPoints[index].y);
-                    //console.log('zooKeeperInstance.data Array', zooKeeperInstance.data)
                     zooKeeperInstance.data.push({
                       x: dataPoints[index].x,
                       y: dataPoints[index].y
@@ -136,29 +113,3 @@ const AvgRequestLatency = () => {
   )
 }
 export default AvgRequestLatency;
-
-
-//{
-//label: 'Dataset 1',
-// backgroundColor: 'rgba(255, 99, 132, 0.5)',
-// borderColor: 'rgb(255, 99, 132)',
-// borderDash: [8, 4],
-// fill: false,
-// data: []
-// },
-// {
-// label: 'Dataset 2',
-// backgroundColor: 'rgba(54, 162, 235, 0.5)',
-// borderColor: 'rgb(54, 162, 235)',
-// cubicInterpolationMode: 'monotone',
-// fill: false,
-// data: []
-// },
-// {
-// label: 'Dataset 3',
-// backgroundColor: 'rgba(50, 100, 100, 0.5)',
-// borderColor: 'rgb(54, 162, 235)',
-// cubicInterpolationMode: 'monotone',
-// fill: false,
-// data: []
-// },
--- hyperion/src/Components/SideNav.jsx
+++ hyperion/src/Components/SideNav.jsx
@@ -1,19 +1,33 @@
-import React from 'react'
-import { Button } from '@mui/material';
+import React, { useState } from 'react'
+import { Button, TextField } from '@mui/material';
 
 const SideNav = () => {
 
-    const button = {
-        color: '#a4a4a4',
-        letterSpacing: '1px'
-    }
+const [connectButton, setConnectButton] = useState(false);
+
+const button = {
+  color: '#a4a4a4',
+  letterSpacing: '1px',
+  width: '120px'
+}
 
     return (
-        <div className="side-nav">
-          <Button variant="text" sx={button}>Connect</Button>
-          <Button variant="text" sx={button}>Error Logs</Button>
-          <Button variant="text" sx={button}>Topics</Button>
-        </div>
+        <>
+          {connectButton && (
+            <div className="side-nav">
+              <TextField className="input" label="Domain" variant="filled" sx={button}/>
+              <TextField className="input" label="Port" variant="filled" sx={button}/>
+              <Button variant="text" sx={button} onClick={()=> setConnectButton(false)}>Submit</Button>
+            </div>
+          )}
+          {!connectButton && (
+            <div className="side-nav">
+              <Button variant="text" sx={button} onClick={()=> setConnectButton(true)}>Connect</Button>
+              <Button variant="text" sx={button}>Error Logs</Button>
+              <Button variant="text" sx={button}>Topics</Button>
+          </div> 
+          )}
+        </>
     )
 };
 
