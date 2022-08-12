af07d2eedcebb5151ee9f017f882f510b575e6a0
Anita Duong <anitaduong98@gmail.com>
2022-08-12 12:24:48 -0400

    front end error log connected

--- hyperion/src/Containers/ErrorLogContainer.jsx
+++ hyperion/src/Containers/ErrorLogContainer.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -6,6 +6,10 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import axios from 'axios';
+
+
+
 
 function createData(name, calories, fat, carbs, protein) {
   return { name, calories, fat, carbs, protein };
@@ -19,34 +23,62 @@ const rows = [
   createData('Gingerbread', 356, 16.0, 49, 3.9),
 ];
 
+
 export default function ErrorLogContainer() {
+
+  const [errorData, setErrorData] = useState(
+    [
+      {
+        error_id: '',
+        instance: '',
+        env: '',
+        value: '',
+        name: '',
+        time: '',
+        user_id: ''
+      }
+    ]
+  );
+
+  useEffect(()=> {
+    // make Initial Fetch on Component Did Mount that returns the data that will be passed into makeDataSets function
+    getErrorData();
+
+  }, [])
+
+  
+  const getErrorData = async () => {
+    const result = await axios.get('/server/errors')
+    console.log('get request', result);
+    setErrorData(result.data);
+  }
+
+
+
+  //console.log('setError state', errorData);
+
   return (
     <TableContainer component={Paper}>
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
           <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+            <TableCell>Error ID</TableCell>
+            <TableCell align="right">Instance</TableCell>
+            <TableCell align="right">Environment</TableCell>
+            <TableCell align="right">Value</TableCell>
+            <TableCell align="right">Name</TableCell>
+            <TableCell align="right">Time</TableCell>
           </TableRow>
         </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <TableRow
-              key={row.name}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
-                {row.name}
-              </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
-            </TableRow>
-          ))}
+        <TableBody>{errorData.map((row) => (
+          <TableRow key={row.error_id} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+            <TableCell component="th" scope="row">{row.error_id}</TableCell>
+            <TableCell align="right">{row.instance}</TableCell>
+            <TableCell align="right">{row.env}</TableCell>
+            <TableCell align="right">{row.value}</TableCell>
+            <TableCell align="right">{row.name}</TableCell>
+            <TableCell align="right">{row.time}</TableCell>
+          </TableRow>))}
         </TableBody>
       </Table>
     </TableContainer>
