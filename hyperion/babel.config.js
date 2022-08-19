b17fbf118f8af4e25f84d30b649c26749d9bfb12
Anita Duong <anitaduong98@gmail.com>
2022-08-19 17:07:16 -0400

    Fixing babel module issues

--- hyperion/babel.config.js
+++ hyperion/babel.config.js
@@ -2,9 +2,11 @@ module.exports = {
   presets: [
     ['@babel/preset-env', {targets: {node: 'current'}}],
     '@babel/preset-react',
-    '@babel/plugin-transform-typescript'
-  ]
+    
+  ],
+  plugins: ['@babel/plugin-transform-typescript']
 };
+
 
   // {
   //   "presets": ["@babel/preset-env", "@babel/preset-react"]
